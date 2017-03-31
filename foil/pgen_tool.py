b544787cc5190e83db610247b74adfe5d1c7c6e4
Andy Chu <andy@oilshell.org>
2017-03-30 18:28:33 -0700

    hook up the lexer

--- foil/pgen_tool.py
+++ foil/pgen_tool.py
@@ -8,6 +8,26 @@ import sys
 from foil.pgen2 import pgen
 from foil import py_ast
 
+import tokenize  # from stdlib?
+
+
+class Parser:
+  def __init__(self, grammar):
+    self.grammar = grammar  # string name -> pgen_ast.term
+
+  def Parse(self, start_symbol, lexer):
+    # TODO:
+    pass
+
+
+class Lexer:
+  def __init__(self, f):
+    self.f = f
+    self._lexer = tokenize.generate_tokens(f.readline)
+
+  def Read(self):
+    return self._lexer.__next__()
+
 
 def main(argv):
   try:
@@ -15,11 +35,18 @@ def main(argv):
   except IndexError:
     f = sys.stdin
   else:
-    f= open(grammar_path)
+    f = open(grammar_path)
 
-  p = pgen.PgenParser(f)
-  t, start_symbol = p.parse()
-  print(t)
+  try:
+    prog_path = argv[2]
+  except IndexError:
+    raise
+  else:
+    prog_f = open(prog_path)
+
+  pg = pgen.PgenParser(f)
+  lang_grammar, start_symbol = pg.parse()
+  print(lang_grammar)
   print(start_symbol)
 
   # TODO: Now interpret the grammar somehow.
@@ -30,7 +57,21 @@ def main(argv):
   # TODO: For clarity, maybe write your own regex pgen tokenizer.  It's
   # trivial.
 
+  # TODO: Get a rule dict of names -> rhs.
+  # 
+
   print(py_ast)
+
+
+  lexer = Lexer(prog_f)
+  for i in range(10):
+    print(lexer.Read())
+  return
+
+
+  py = Parser(lang_grammar)
+  print(py)
+  py.Parse(start_symbol, lexer)
 
 
 if __name__ == '__main__':
@@ -39,4 +80,3 @@ if __name__ == '__main__':
   except RuntimeError as e:
     print('FATAL: %s' % e, file=sys.stderr)
     sys.exit(1)
-
